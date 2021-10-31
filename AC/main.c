d4cabacc6335af47b34b6f45a88894c8b7632872
mrsahin101 <m.rasitsahin@hotmail.com>
2021-10-31 18:38:29 +0300

    Simple Ac simulation program

---
+++ AC/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sensor.h"
+#include "motor.h"
+
+uint32_t temperature = 25;
+
+int main(int argc, char *argv[]) {
+	
+	motor_init();
+	sensor_init();
+	
+	while(1)
+	{
+		sensor_read();
+		mydelay();
+		if(temperature > 30)
+		{
+			cooling_motor_on();
+		}
+		if(temperature < 20)
+		{
+			heating_motor_on();
+		}
+	}
+	return 0;
+}
+void mydelay(void)
+{
+	long int i= 0;
+	for(i = 0; i < 400000000;i++)
+	{
+		
+	}
+	return;
+}
---
+++ AC/motor.c
@@ -0,0 +1,25 @@
+#include "motor.h"
+#include "main.h"
+#include <stdio.h>
+void cooling_motor_on(void)
+{
+	while(temperature >= 25)
+	{
+		printf("Cooling system is on. Current Temperature : %d\n",temperature);
+		temperature--;
+		mydelay();
+	}
+}
+void heating_motor_on(void)
+{
+	while(temperature <= 20)
+	{
+		printf("Heating system is on. Current Temperature : %d\n",temperature);
+		temperature++;
+		mydelay();
+	}
+}
+void motor_init(void)
+{
+	printf("AC cooling--heating system has been initialized..\n");
+}
